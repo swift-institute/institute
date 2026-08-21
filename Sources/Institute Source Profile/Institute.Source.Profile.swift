5c063a85b5ae298210702be27789340db71eb3d0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 22:40:10 +0200

    Qualify source profile owners

--- Sources/Institute Source Profile/Institute.Source.Profile.swift
+++ Sources/Institute Source Profile/Institute.Source.Profile.swift
@@ -7,24 +7,27 @@ public import Source_Profile
 
 extension Institute.Source {
     public struct Profile: Sendable {
-        public let policy: Institute.ContinuousIntegration.Source.Policy
+        public let policy:
+            Institute_Continuous_Integration_Source.Institute.ContinuousIntegration.Source.Policy
 
         public init(
-            policy: Institute.ContinuousIntegration.Source.Policy = .current
+            policy:
+                Institute_Continuous_Integration_Source.Institute.ContinuousIntegration.Source.Policy = .current
         ) {
             self.policy = policy
         }
 
         public func rules(
-            for bundle: Institute.ContinuousIntegration.Source.Bundle
-        ) -> [SourceDomain.Rule.ID] {
+            for bundle:
+                Institute_Continuous_Integration_Source.Institute.ContinuousIntegration.Source.Bundle
+        ) -> [Source_Profile.Source.Rule.ID] {
             let configurations: [Lint.Rule.Configuration]
             switch bundle {
             case .primitives: configurations = Lint.Rule.Bundle.primitives
             case .standards: configurations = Lint.Rule.Bundle.standards
             case .institute: configurations = Lint.Rule.Bundle.institute
             }
-            let engine = SourceDomain.Engine.ID("swift-linter")
+            let engine = Source_Profile.Source.Engine.ID("swift-linter")
             return configurations.map {
                 .init(engine: engine, token: $0.rule.id.underlying)
             }.sorted(by: { $0.token < $1.token })
